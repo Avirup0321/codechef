34500095c5bd87aae12360de45500f5ef22cc734
Avirup Chattopadhyay <55937969+Avirup0321@users.noreply.github.com>
2021-02-21 08:05:27 +0530

    Add Two Numbers
    Input
    
    The first line contains an integer T, the total number of test cases. Then follow T lines, each line contains two Integers A and B
    3 
    1 2
    100 200
    10 40
    
    
    Output.
    3
    300
    50

---
+++ addition.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+    int t,a,b,i,c;
+    cin>>t;
+
+    for (i=1;i<=t;i++)
+    {
+        cin>>a>>b;
+        cout<< a+b;
+    }
+
+      
+    
+        cout<< c;
+       
+}
