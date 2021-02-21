fafc175536a692b66288fedc3f2ed4d2fbce5f8c
Avirup Chattopadhyay <55937969+Avirup0321@users.noreply.github.com>
2021-02-22 01:51:00 +0530

    Find Remainder
    Input
    The first line contains an integer T, the total number of test cases. Then T lines follow, each line contains two Integers A and B.
    
    Output
    For each test case, find the remainder when A is divided by B, and display it in a new line.
    
    Input
    3 
    1 2
    100 200
    40 15
    
    Output
    1
    100
    10

---
+++ remainder.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+int i,a,n,b,rem=0;
+while (t--)
+{
+    cin>>a>>b;
+    rem = a%b;
+    cout << rem<< endl;
+}
+
+}
